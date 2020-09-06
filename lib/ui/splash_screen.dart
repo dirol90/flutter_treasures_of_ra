34ce939cfba6de4ba68f59ace85d9b5a5088e9b4
dirol90@gmail.com <dirol90@gmail.com>
2020-09-06 19:03:42 +0100

    - app encryptor

--- lib/ui/splash_screen.dart
+++ lib/ui/splash_screen.dart
@@ -19,12 +19,15 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   Timer _timer;
   final cryptor = new PlatformStringCryptor();
-  final password = "aMHp7UwTeAUf7JYTi+Opcg==:aOTvlL2z6iP/2DPczLdBr0jJMp1bmIOO6Cp9LMcr5jQ=";
+  final password =
+      "aMHp7UwTeAUf7JYTi+Opcg==:aOTvlL2z6iP/2DPczLdBr0jJMp1bmIOO6Cp9LMcr5jQ=";
+
+  String lastURLText = '';
+  String lastEncryptText = '';
 
   @override
   Widget build(BuildContext context) {
-
-    FacebookDeeplinks().onDeeplinkReceived.listen(_onRedirected);
+    // FacebookDeeplinks().onDeeplinkReceived.listen(_onRedirected);
 
 //    FacebookDeeplinks().getInitialUrl().then((value) async {
 //      print('11 $value');
@@ -33,22 +36,21 @@ class _SplashScreenState extends State<SplashScreen> {
 //      });
 //    });
 
-    _timer = Timer(Duration(seconds: 5), () {
-      nextScreen(context);
-      _timer.cancel();
-    });
-
+    // _timer = Timer(Duration(seconds: 5), () {
+    //   nextScreen(context);
+    //   _timer.cancel();
+    // });
 
     return Scaffold(
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/elements/bg.png"),
-            fit: BoxFit.cover,
-          ),
-        ),
+        // decoration: BoxDecoration(
+        //   image: DecorationImage(
+        //     image: AssetImage("assets/elements/bg.png"),
+        //     fit: BoxFit.cover,
+        //   ),
+        // ),
         child: SafeArea(
           child: Stack(
             children: <Widget>[
@@ -57,11 +59,26 @@ class _SplashScreenState extends State<SplashScreen> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
-                    Image.asset('assets/elements/app_logo.png', height: MediaQuery.of(context).size.width/3*2, width: MediaQuery.of(context).size.width/3*1.5,),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 24.0),
-                      child: Image.asset('assets/elements/app_name.png', width: MediaQuery.of(context).size.width/3*1,),
-                    )
+                    TextField(
+                      onChanged: (text) {
+                        lastURLText = text;
+                      },
+                      decoration: InputDecoration(
+                          border: InputBorder.none, hintText: 'Put Link'),
+                    ),
+                    FlatButton(
+                      onPressed: () {
+                        _encrypt(lastURLText);
+                      },
+                      child: Text(
+                        'Convert URL',
+                      ),),
+                    SelectableText(lastEncryptText),
+                    // Image.asset('assets/elements/app_logo.png', height: MediaQuery.of(context).size.width/3*2, width: MediaQuery.of(context).size.width/3*1.5,),
+                    // Padding(
+                    //   padding: const EdgeInsets.only(top: 24.0),
+                    //   child: Image.asset('assets/elements/app_name.png', width: MediaQuery.of(context).size.width/3*1,),
+                    // )
                   ],
                 ),
               ),
@@ -72,21 +89,24 @@ class _SplashScreenState extends State<SplashScreen> {
     );
   }
 
-  void nextScreen(BuildContext context){
-//    _encrypt('URL_HERE');
+  void nextScreen(BuildContext context) {
+    // _encrypt('');
     _getSharedPref().then((value) {
-
-    if (value == null || value.isEmpty){
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => MainScreen()));
-    } else {
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => WebViewScreen(url: value)));
-    }
+      if (value == null || value.isEmpty) {
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (BuildContext context) => MainScreen()));
+      } else {
+        Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+                builder: (BuildContext context) => WebViewScreen(url: value)));
+      }
     });
   }
 
   Future<void> _onRedirected(String url) async {
     _setSharedPref(await _decrypt(url)).then((value) {
-    nextScreen(context);
+      nextScreen(context);
     });
   }
 
@@ -103,12 +123,16 @@ class _SplashScreenState extends State<SplashScreen> {
 
   Future<void> _encrypt(String s) async {
     final String encrypted = await cryptor.encrypt(s, password);
+    setState(() {
+      lastEncryptText = encrypted;
+    });
     print("ENCRYOTED: $encrypted");
     _decrypt(encrypted);
   }
 
   Future<String> _decrypt(String encrypted) async {
-    encrypted = encrypted.replaceRange(encrypted.indexOf('?al_applink_data'), encrypted.length, '');
+    encrypted = encrypted.replaceRange(
+        encrypted.indexOf('?al_applink_data'), encrypted.length, '');
     if (encrypted != null) {
       try {
         final String decrypted = await cryptor.decrypt(encrypted, password);
@@ -121,8 +145,5 @@ class _SplashScreenState extends State<SplashScreen> {
     } else {
       return '';
     }
-
   }
-
 }
-
